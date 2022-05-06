99b750c792ac741e1d90e036b660bdb03dbe270e
gersonline <gersonline@outlook.com.br>
2022-05-06 02:13:32 -0300

    Cruds Empresa e Funcionario

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import ListView, UpdateView
+from .models import Funcionario
 
-def home(request):
-    return  HttpResponse('Ola')
+
+class FuncionariosList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+
+    fields = ['nome', 'departamentos']
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
