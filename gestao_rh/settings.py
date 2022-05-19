1a01730cddd524936a6cce5c9e4d742a550f018d
gersonline <gersonline@outlook.com.br>
2022-05-19 20:15:42 -0300

    STATIC_ROOT TryFix

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -13,7 +13,7 @@ import os.path
 from pathlib import Path
 from django.apps import AppConfig
 
-#AppConfig.default = False
+AppConfig.default = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,14 +129,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-'''
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-'''
 
 
-STATIC_ROOT = os.path.join('/home/ubuntu/Gestao_RH/static/')
+
+#STATIC_ROOT = os.path.join('/home/ubuntu/Gestao_RH/static/')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
