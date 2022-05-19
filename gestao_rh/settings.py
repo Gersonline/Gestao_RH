76e241262f985a2c1d7c1b4dc0167a499dc0b634
gersonline <gersonline@outlook.com.br>
2022-05-19 19:10:55 -0300

    STATIC_ROOT TryFix

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -133,9 +133,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
