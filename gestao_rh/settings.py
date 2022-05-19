fd441a769a67d939a22dbc5dfc468e855a01bfa7
gersonline <gersonline@outlook.com.br>
2022-05-19 20:25:25 -0300

    STATIC_ROOT TryFix

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -128,14 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-'''
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-'''
 
 
-STATIC_ROOT = os.path.join('/home/ubuntu/Gestao_RH/static/')
+
+#STATIC_ROOT = os.path.join('/home/ubuntu/Gestao_RH/static/')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
